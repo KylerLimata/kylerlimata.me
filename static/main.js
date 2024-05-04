644d6a71c2c6a32754b3cac8b57927dfbfc6ea0e
KylerLimata <kyler.limata@gmail.com>
2024-05-04 16:44:14 -0700

    Gallery buttons now over image

--- static/main.js
+++ static/main.js
@@ -15,20 +15,53 @@ class GalleryElement extends HTMLElement {
         const gallery = this
         const shadow = this.attachShadow({ mode: 'open' });
         const style = document.createElement("style");
+        const container = document.createElement("div");
         const img = this.img
         const button_left = document.createElement("button");
         const button_right = document.createElement("button");
 
         style.textContent = `
+            .container {
+                position: relative; /* set container to a relative position */
+                width: 100%
+            }
+
             img {
                 width: 100%;
             }
+
+            .left-button {
+                position: absolute; /* set button to an absolute position */
+                top: 50%; /* adjust the top position of the button */
+                left: 0%; /* adjust the left position of the button */
+                transform: translate(0%, -50%); /* center the button */
+                z-index: 2; /* set a higher z-index than the image */
+                color: #fff; /* set your desired text color */
+                cursor: pointer; /* change mouse cursor on hover */
+            }
+
+            .right-button {
+                position: absolute; /* set button to an absolute position */
+                top: 50%; /* adjust the top position of the button */
+                left: 100%; /* adjust the left position of the button */
+                transform: translate(-100%, -50%); /* center the button */
+                z-index: 2; /* set a higher z-index than the image */
+                color: #fff; /* set your desired text color */
+                cursor: pointer; /* change mouse cursor on hover */
+            }
         `
 
-        shadow.appendChild(img)
-        shadow.appendChild(style)
-        shadow.appendChild(button_left)
-        shadow.appendChild(button_right)
+        shadow.appendChild(container);
+        container.appendChild(img);
+        container.appendChild(style);
+        container.appendChild(button_left);
+        container.appendChild(button_right);
+
+        container.classList = "container"
+        button_left.classList = "left-button"
+        button_left.textContent = "<"
+        button_right.textContent = ">"
+        button_right.classList = "right-button"
 
         fetch(this.getAttribute("src"))
             .then(res => res.json())
